2349b47c77b733c51a7b08cf24ef9bef7a4fbd36
meghapradhan <83629903+meghapradhan@users.noreply.github.com>
2021-06-25 16:45:34 +0530

    Add files via upload

---
+++ pdf.js
@@ -0,0 +1,11 @@
+$('#downloadPDF').click(function () {
+    domtoimage.toPng(document.getElementById('content2'))
+        .then(function (blob) {
+            var pdf = new jsPDF('l', 'pt', [$('#content2').width(), $('#content2').height()]);
+
+            pdf.addImage(blob, 'PNG', 0, 0, $('#content2').width(), $('#content2').height());
+            pdf.save("test.pdf");
+
+            that.options.api.optionsChanged();
+        });
+});
